ebc3ac899f887cf3809c2164bfc903d2383ebaea
sivarayavaram <58628752+sivarayavaram@users.noreply.github.com>
2020-01-05 04:36:23 -0500

    Add files via upload

---
+++ Imagecompression_PCA.R
@@ -0,0 +1,24 @@
+library(jpeg)
+library(imager)
+library(pixmap)
+library(pixmapRGB)
+cat <- readJPEG('/cat.jpg')
+
+ncol(cat)
+nrow(cat)
+
+r <- cat[,,1]
+g <- cat[,,2]
+b <- cat[,,3]
+
+cat.r.pca <- prcomp(r, center = FALSE)
+cat.g.pca <- prcomp(g, center = FALSE)
+cat.b.pca <- prcomp(b, center = FALSE)
+
+rgb.pca <- list(cat.r.pca, cat.g.pca, cat.b.pca)
+pca.img <- sapply(rgb.pca, function(j) {  compressed.img <- j$x[,1:10] %*% t(j$rotation[,1:10])}, simplify = 'array')
+a<-pixmap::pixmapRGB(pca.img)
+windows()
+plot(a)
+
+
