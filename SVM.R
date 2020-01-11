d331bcf25f50f62fcb095b8e840c6325d4bae5ef
sivarayavaram <58628752+sivarayavaram@users.noreply.github.com>
2020-01-11 04:47:29 -0500

    Add files via upload

---
+++ SVM.R
@@ -0,0 +1,30 @@
+##### SVM#####
+# divide into training and test data
+letters_train <- letterdata[1:16000, ]
+letters_test  <- letterdata[16001:20000, ]
+
+##Training a model on the data ----
+# begin by training a simple linear SVM
+library(kernlab)
+letter_classifier <- ksvm(letter ~ ., data = letters_train,
+                          kernel = "vanilladot")
+## Evaluating model performance ----
+# predictions on testing dataset
+letter_predictions <- predict(letter_classifier, letters_test)
+
+head(letter_predictions)
+
+table(letter_predictions, letters_test$letter)
+
+
+agreement <- letter_predictions == letters_test$letter
+table(agreement)
+prop.table(table(agreement))
+
+## Improving model performance ----
+letter_classifier_rbf <- ksvm(letter ~ ., data = letters_train, kernel = "rbfdot")
+letter_predictions_rbf <- predict(letter_classifier_rbf, letters_test)
+
+agreement_rbf <- letter_predictions_rbf == letters_test$letter
+table(agreement_rbf)
+prop.table(table(agreement_rbf))
